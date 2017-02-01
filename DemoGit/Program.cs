dd0403c7697d354189404dffd6702fc3feddb0ee
G Paquette <gabrielpaquette98@gmail.com>
2017-02-01 08:46:41 -0500

    Commentaire ajouté

--- DemoGit/Program.cs
+++ DemoGit/Program.cs
@@ -10,7 +10,8 @@ namespace DemoGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            //commenya
+            Console.WriteLine("git gud");
         }
     }
 }
